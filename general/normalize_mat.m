efb39f5d858163fbddef4047491e5bcb998b8802
arianasp <arianasp@gmail.com>
2014-03-21 15:16:12 -0400

    updated normalize_mat func to include functionality for normalizing over both rows and cols

--- general/normalize_mat.m
+++ general/normalize_mat.m
@@ -5,9 +5,9 @@ function [ norm_mat ] = normalize_mat( mat, norm_dim, norm_stat )
 %   mat: [NxM matrix]
 %   norm_dim: [string] dimension to normalize on 
 %       ('rows'/'row' or 'cols'/'col')
-%   norm_stat: what to normalize based on (what to divide each row/column
-%       by - defaults to 'sum' but can also be 'mean', 'median', 
-%       'max' or 'min')
+%   norm_stat: what to normalize based on (what to divide each element
+%       by - defaults to 'sum' i.e. sum of row or column or both, but can 
+%       also be 'mean', 'median', 'max' or 'min')
 %OUTPUTS:
 %   norm_mat: normalize matrix,  as described above
 %Note about NaNs: ignores NaNs in creating sums, but preserves existing NaN
@@ -22,22 +22,28 @@ if strcmp(norm_dim,'row') || strcmp(norm_dim,'rows')
     dim = 2;
 elseif strcmp(norm_dim,'col') || strcmp(norm_dim,'cols')
     dim = 1;
+elseif strcmp(norm_dim,'both')
+    if not(strcmp(norm_stat,'sum'))
+        error('normalization dimension "both" is incompatible with anything but normalization statistic "sum"')
+    end
 else
     error('unknown normalization dimension')
 end
 
-if strcmp(norm_stat,'sum')
-    denoms = nansum(mat,dim);
-elseif strcmp(norm_stat,'mean')
-    denoms = nanmean(mat,dim);
-elseif strcmp(norm_stat,'median')
-    denoms = nanmedian(mat,dim);
-elseif strcmp(norm_stat,'max')
-    denoms = nanmax(mat,[],dim);
-elseif strcmp(norm_stat,'min')
-    denoms = nanmin(mat,[],dim);
-else
-    error('unknown normalization statistic')
+if not(strcmp(norm_dim,'both'))
+    if strcmp(norm_stat,'sum')
+        denoms = nansum(mat,dim);
+    elseif strcmp(norm_stat,'mean')
+        denoms = nanmean(mat,dim);
+    elseif strcmp(norm_stat,'median')
+        denoms = nanmedian(mat,dim);
+    elseif strcmp(norm_stat,'max')
+        denoms = nanmax(mat,[],dim);
+    elseif strcmp(norm_stat,'min')
+        denoms = nanmin(mat,[],dim);
+    else
+        error('unknown normalization statistic')
+    end
 end
 
 if strcmp(norm_dim,'row') || strcmp(norm_dim,'rows')
@@ -47,6 +53,12 @@ if strcmp(norm_dim,'row') || strcmp(norm_dim,'rows')
 elseif strcmp(norm_dim,'col') || strcmp(norm_dim,'cols')
     for i = 1:N
         norm_mat(:,i) = mat(:,i) / denoms(i);
+    end
+elseif strcmp(norm_dim,'both') 
+    for i = 1:N
+        for j = 1:N
+            norm_mat(i,j) = mat(i,j) / nansum(mat(i,:)) / nansum(mat(:,j));
+        end
     end
 end
 
