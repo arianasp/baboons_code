3d289ac43ebdf7eda02517450c303da6ca7da2e3
arianasp <arianasp@gmail.com>
2014-03-27 16:55:08 -0400

    added funcs to compute speeds and to get the total number of slinky interactions in each time step

---
+++ data_processing/scripts/compute_speeds.m
@@ -0,0 +1,111 @@
+%Compute and save speeds from positions data.
+
+%% Parameters
+
+%time step to use when computing the speeds
+%if this is equal to 1, the speed is found using the difference between
+%positions in consecutive time steps. If this is greater than 1, the speed
+%is found using the difference between positions that are t_step seconds
+%apart.
+t_step = 10;
+
+%filename for the file with xy data
+xy_file = ['/Users/arianasp/Desktop/Baboons/data/matlab_raw/xy_level1.mat'];
+
+%filename for the file with centroid over time data
+centr_file = ['/Users/arianasp/Desktop/Baboons/data/matlab_processed/troop_level_metrics/centroid_min_inds_10_level1.mat'];
+
+%directory in which to save the output file
+savedir = ['/Users/arianasp/Desktop/Baboons/data/matlab_processed/individual_level_metrics'];
+
+%% Load files
+
+disp('loading files...')
+
+load(xy_file)
+load(centr_file)
+
+%% Extract appropriate data
+
+disp('extracting data...')
+
+%number of days
+n_days = length(day_start_idxs);
+
+%add an ending time to day_start_idxs
+day_start_idxs = [day_start_idxs size(xs,2)+1];
+
+%position data (regular and shifted up by t_step)
+%note the precision change to "double" - this is because we are subtracting
+%small numbers to get speeds
+xs = double(xs);
+xs_shift = xs(:,(1+t_step):end);
+xs_curr = xs(:,1:(end-t_step));
+ys = double(ys);
+ys_shift = ys(:,(1+t_step):end);
+ys_curr = ys(:,1:(end-t_step));
+
+troopxs = double(centr_over_time(:,1));
+troopxs_shift = troopxs((1+t_step):end);
+troopxs_curr = troopxs(1:(end-t_step));
+troopys = double(centr_over_time(:,2));
+troopys_shift = troopys((1+t_step):end);
+troopys_curr = troopys(1:(end-t_step));
+
+%% Compute speeds
+
+disp('computing speeds...')
+
+%x and y velocities for individuals
+dxs = (xs_shift - xs_curr)/t_step; 
+dys = (ys_shift - ys_curr)/t_step;
+
+%x and y velocities for troop centroid
+troopdxs = (troopxs_shift - troopxs_curr)/t_step; 
+troopdys = (troopys_shift - troopys_curr)/t_step;
+
+%number of individuals tracked over time
+n_inds = sum(not(isnan(xs)),1); 
+n_inds_shift = n_inds((1+t_step):end);
+n_inds_curr = n_inds(1:(end-t_step));
+
+%change in number of tracked individuals (need to make troop speeds 
+%adjacent to these NaN)
+d_n_inds = n_inds_shift - n_inds_curr; 
+
+%get rid of speeds that cross days (replace with NaNs)
+for d = 1:n_days
+    last = day_start_idxs(d+1) - 1;
+    dxs(:,(last-t_step+1):last) = NaN;
+    dys(:,(last-t_step+1):last) = NaN;
+    troopdxs((last-t_step+1):last) = NaN;
+    troopdys((last-t_step+1):last) = NaN;
+end
+
+%get rid of troop-level speeds where the number of tracked individuals
+%changed (replace with NaNs)
+changing_inds = find(abs(d_n_inds)>0);
+for i = 1:length(changing_inds)
+    idx = changing_inds(i);
+    troopdxs(idx) = NaN;
+    troopdys(idx) = NaN;
+end
+
+%compute speeds for individual and troop level
+troop_speeds = sqrt(troopdxs.^2 + troopdys.^2);
+speeds = sqrt(dxs.^2 + dys.^2);
+
+%% Save data
+
+disp('saving data...')
+
+savename = [savedir '/speeds_step_' num2str(t_step) '_level1.mat'];
+save(savename,'speeds','troop_speeds');
+
+disp('done')
+
+
+
+
+
+
---
+++ spring_interactions/number_of_interactions_over_time.m
@@ -0,0 +1,85 @@
+function [ n_interactions_over_time ] = number_of_interactions_over_time( interactions_all, event_type, t_start, t_end, min_strength, min_disp )
+%Get a timeseries of the number of events that occur between all pairs of 
+%individuals, and store everything in a Tx1 vector
+%INPUTS:
+%   interactions_all: [cell array] containing interaction information 
+%       (contained in structs) for each pair of individuals 
+%       (output of get_dyadic_interactions_all_pairs script)
+%   event_type: [string] representing what type of events to pull out - can
+%       be 'push','pull','anchor', or 'repel'
+%   t_start: [number] the first time index to include
+%   t_end: [number] the last time index to include
+%   min_strength: [number] minimum strength required to count an event
+%       (defaults to 0)
+%   min_disp: [number] minimum disaprity required to count an event
+%       (defaults to 0)
+%OUTPUTS:
+%   n_interactions_over_time: [Tx1 vector] giving the number of ongoing
+%       interactions (over all pairs of individuals) at a given time
+
+%get number of individuals
+N = size(interactions_all,1);
+
+%check that the data is in the correct form (NxN cell array)
+if size(interactions_all,2) ~= N
+    error('interactions_all must be a square cell array')
+end
+
+%check that the event type is one of the supported ones
+if not(strcmp(event_type,'push') || strcmp(event_type,'pull') || ...
+        strcmp(event_type,'anchor') || strcmp(event_type,'repel'))
+    error('event_type must be "push","pull","anchor", or "repel"')
+end
+
+%default min_strength = 0
+if ~exist('min_strength')
+    min_strength = 0;
+end
+
+%default min_disp = 0
+if ~exist('min_disp')
+    min_disp = 0;
+end
+
+%create an array to hold timeseries data
+n_interactions_over_time = zeros(t_end-t_start+1,1);
+
+%get interactions and put them in the matrix
+for i = 1:N
+    for j = (i+1):N
+        
+        %get interactions for each pair
+        interactions = interactions_all{i,j};
+        
+        %for each interaction
+        for k = 1:length(interactions)
+            
+            t0 = interactions(k).time_idxs(1);
+            tf = interactions(k).time_idxs(3);
+            
+            %if within the time range
+            if t0 >= t_start || tf <= t_end
+                
+            
+                %get its type
+                curr_type = interactions(k).type;
+            
+                %count it if it's the correct type and passes the thresholds
+                if strcmp(curr_type,event_type)
+                    strength = interactions(k).strength_mult;
+                    disp = interactions(k).disp_mult;
+                    if strength >= min_strength && disp >= min_disp
+                        t_event_begin = max(t0,t_start);
+                        t_event_end = min(tf,t_end);
+                        start_idx = t_event_begin - t_start + 1;
+                        end_idx = t_event_end - t_start + 1;
+                        n_interactions_over_time(start_idx:end_idx) = n_interactions_over_time(start_idx:end_idx) + 1;
+                    end
+                end
+            end
+        end
+    end
+end
+
+end
+
