e91f09dd010db14f06cdaf6a324c55ab0470abd6
Sam I. Am <samiam@nat-oitwireless-inside-vapornet100-b-31071.princeton.edu>
2014-04-16 10:14:30 -0400

    fixed bug in the binning

--- entropy/mutual_information.m
+++ entropy/mutual_information.m
@@ -62,35 +62,38 @@ X(nans,:) = [];
 Y(nans,:) = [];
 
 %bins for dimension 1
-x1 = transpose(linspace(mins(1)-(maxes(1)-mins(1))/nbins(1)/2, maxes(1)+(maxes(1)-mins(1))/nbins(1)/2,nbins(1)));
+x1 = transpose(linspace(mins(1)-(maxes(1)-mins(1))/(nbins(1)-2)/2, maxes(1)+(maxes(1)-mins(1))/(nbins(1)-2)/2,nbins(1)));
 
 %interpolaed values for x and y in dimension 1
-xr1 = interp1(x1,1:numel(x1),X(:,1),'nearest');
-yr1 = interp1(x1,1:numel(x1),Y(:,1),'nearest');
+xr1 = interp1(x1,0:(numel(x1)-1),X(:,1),'nearest');
+yr1 = interp1(x1,0:(numel(x1)-1),Y(:,1),'nearest');
 
 if D == 2
     %bins for dimension 2
-    x2 = transpose(linspace(mins(2)-(maxes(2)-mins(2))/nbins(2)/2, maxes(2)+(maxes(2)-mins(2))/nbins(2)/2,nbins(2)));
+    x2 = transpose(linspace(mins(2)-(maxes(2)-mins(2))/(nbins(2)-2)/2, maxes(2)+(maxes(2)-mins(2))/(nbins(2)-2)/2,nbins(2)));
     %interpolated values for x and y in dimension 2
-    xr2 = interp1(x2,1:numel(x2),X(:,2),'nearest');
-    yr2 = interp1(x2,1:numel(x2),Y(:,2),'nearest');
+    xr2 = interp1(x2,0:(numel(x2)-1),X(:,2),'nearest');
+    yr2 = interp1(x2,0:(numel(x2)-1),Y(:,2),'nearest');
 end
 
 %remove out of range data
-nans = find(isnan(xr1));
-nans = union(nans,find(isnan(yr1)));
+oor_x1 = union(find(X(:,1)<mins(1)),find(X(:,1)>maxes(1)));
+oor_y1 = union(find(Y(:,1)<mins(1)),find(Y(:,1)>maxes(1)));
+oor = union(oor_x1,oor_y1);
 
 if D == 2
-    nans = union(nans,find(isnan(xr2)));
-    nans = union(nans,find(isnan(yr2)));
+    oor_x2 = union(find(X(:,2)<=mins(2)),find(X(:,2)>=maxes(2)));
+    oor_y2 = union(find(Y(:,2)<=mins(2)),find(Y(:,2)>=maxes(2)));
+    oor2 = union(oor_x2,oor_y2);
+    oor = union(oor,oor2);
 end
 
-xr1(nans) = [];
-yr1(nans) = [];
+xr1(oor) = [];
+yr1(oor) = [];
 
 if D == 2
-    xr2(nans) = [];
-    yr2(nans) = [];
+    xr2(oor) = [];
+    yr2(oor) = [];
 end
 
 n_times = length(xr1);
