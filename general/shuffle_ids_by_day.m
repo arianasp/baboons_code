1e974ffc2c9330f5ac05108f22caed99ffbfe064
Sam I. Am <samiam@Sams-MacBook-Pro.local>
2014-04-21 15:32:50 -0500

    updated shuffle_ids_by_day to deal with multiple data dimensions

--- general/shuffle_ids_by_day.m
+++ general/shuffle_ids_by_day.m
@@ -3,8 +3,11 @@ function [ data_shuffled ] = shuffle_ids_by_day( data, day_start_idxs )
 %columns of the NxT matrix 'data', independently for each day given in
 %day_start_idxs.
 %INPUTS:
-%   data: [N x T matrix] of values for each individual over time (e.g. xs
-%       or ys matrix
+%   data: [N x T x n_dim matrix] of values for each individual over time 
+%       (e.g. xs or ys matrix). n_dim is the number of dimensions in the
+%       data, for example you could enter an N x T x 2 matrix that contains
+%       BOTH the x and y values for each individual, if you wanted to
+%       shuffle the data such that the x and y values remained coupled
 %   day_start_idxs: [1 x n_days vector] of the index to the start of each day
 %OUTPUTS:
 %   data_shuffled = [N x T matrix] of the same values, with the individual
@@ -13,6 +16,7 @@ function [ data_shuffled ] = shuffle_ids_by_day( data, day_start_idxs )
 %get size of data matrix
 N = size(data,1);
 T = size(data,2);
+n_dim = size(data,3);
 
 %number of days
 n_days = length(day_start_idxs);
@@ -21,11 +25,11 @@ n_days = length(day_start_idxs);
 day_start_idxs = [day_start_idxs T+1];
 
 %shuffle data by day
-data_shuffled = nan(N,T);
+data_shuffled = nan(N,T,n_dim);
 for d = 1:n_days
-    curr_dat = data(:,day_start_idxs(d):(day_start_idxs(d+1)-1));
-    curr_dat = curr_dat(randperm(N),:);
-    data_shuffled(:,day_start_idxs(d):(day_start_idxs(d+1)-1)) = curr_dat;
+    curr_dat = data(:,day_start_idxs(d):(day_start_idxs(d+1)-1),:);
+    curr_dat = curr_dat(randperm(N),:,:);
+    data_shuffled(:,day_start_idxs(d):(day_start_idxs(d+1)-1),:) = curr_dat;
 end
 
 
