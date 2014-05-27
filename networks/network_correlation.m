b470d35ca04870543cee025e91b650cfcaa78516
arianasp <arianasp@gmail.com>
2014-05-27 10:43:46 -0500

    modified some network functions

--- networks/network_correlation.m
+++ networks/network_correlation.m
@@ -1,17 +1,20 @@
-function [ R ] = network_correlation( A1, A2, corr_type, directed )
+function [ R ] = network_correlation( A1, A2, corr_type, net_type )
 %Computes the correlation between two networks, described by adjacency
 %matrix A1 and A2, using a correlation of type corr_type. If the networks
-%are directed (directed == 1), the correlation is over all entries in the 
-%matrices (excluding the diagonal). If they are not (directed == 0), then 
-%only the upper triangle is used (to avoid double counting).
-%For now, only the Pearson correlation is supported, but I may be adding
-%more functionality later.
+%are directed (net_type 'directed'), the correlation is over all entries in the 
+%matrices (excluding the diagonal). If they are undirected (net_type 
+%'undirected' or 'antisymmetric'), then  only the upper triangle is used 
+%(to avoid double counting). For now, only the Pearson correlation is 
+%supported, but I may be adding more functionality later.
 %INPUTS:
 %   A1: [N x N matrix] of network connections 
 %   A2: [N x N matrix] of network connections
 %   corr_type: [string] type of correlation to use. Right now, supported
 %   types are: 'Pearson'
-%   directed: [bool] whether the network is directed (1) or undirected (0)
+%   net_tye: [string] whether the network is 'directed' (in which case the 
+%       correlation is run over all entries) or 'undirected'/'symmetric', 
+%       or 'antisymmetric' (in which case the correlation is run only over
+%       the upper triangular entries)
 %OUTPUTS:
 %   R: value of the correlation
 
@@ -26,12 +29,13 @@ end
 %if undirected, use only upper triangular portion
 uppertri = triu(ones(N),1);
 lowertri = tril(ones(N),1);
-if directed
-    edges1 = [A1(find(uppertri)); A1(find(lowertri))];
-    edges2 = [A2(find(uppertri)); A2(find(lowertri))];
-else
-    edges1 = A1(find(uppertri));
-    edges2 = A2(find(uppertri));
+switch net_type
+    case 'directed'
+        edges1 = [A1(find(uppertri)); A1(find(lowertri))];
+        edges2 = [A2(find(uppertri)); A2(find(lowertri))];
+    case {'undirected' 'symmetric' 'antisymmetric' 'anti-symmetric'}
+        edges1 = A1(find(uppertri));
+        edges2 = A2(find(uppertri));
 end
 
 if strcmp(corr_type,'Pearson') || strcmp(corr_type,'pearson')
--- networks/network_correlation_sig_test.m
+++ networks/network_correlation_sig_test.m
@@ -1,4 +1,4 @@
-function [ stats ] = network_correlation_sig_test( A1, A2, corr_type, net_type, n_randomizations )
+function [ stats ] = network_correlation_sig_test( A1, A2, corr_type, net_type, n_randomizations, display_plot )
 %Computes the network correlation and performs a randomization test to test
 %the significance of the result.
 %INPUTS:
@@ -10,10 +10,18 @@ function [ stats ] = network_correlation_sig_test( A1, A2, corr_type, net_type, 
 %       correlation is run over all entries) or 'undirected'/'symmetric', 
 %       or 'antisymmetric' (in which case the correlation is run only over
 %       the upper triangular entries)
+%   n_randomizations: [number] number of randomizations to do
+%   display_plot: [bool] whether to create a plot of edge strength in A1 vs
+%       edge strength in A2 (and draw a line with a slope of R) - defaults
+%       to 0
 %OUTPUTS:
 %   stats: [struct] with fields
 %       .R: value of the correlation
 %       .p: p-value
+
+if ~exist('display_plot')
+    display_plot = 0;
+end
 
 %get correlation between the two networks
 [ R_data ] = network_correlation( A1, A2, corr_type, net_type );
@@ -28,6 +36,24 @@ p = sum(R_null < R_data) / n_randomizations;
 
 stats.R = R_data;
 stats.p = p;
+
+if display_plot
+    N = size(A1,1);
+    uppertri = triu(ones(N),1);
+    lowertri = tril(ones(N),1);
+    switch net_type
+        case 'directed'
+            edges1 = [A1(find(uppertri)); A1(find(lowertri))];
+            edges2 = [A2(find(uppertri)); A2(find(lowertri))];
+        case {'undirected' 'symmetric' 'antisymmetric' 'anti-symmetric'}
+            edges1 = A1(find(uppertri));
+            edges2 = A2(find(uppertri));
+    end
+    plot(edges1,edges2,'.','MarkerSize',10)
+    
+end
+
+
 
 
 end
